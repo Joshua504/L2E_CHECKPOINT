ec25f006e0602e48dfbb4e7f7867c2841dfd93b8
joshua504 <chukwuebukajosh8@gmail.com>
2026-06-26 06:56:19 +0100

    more

--- main.go
+++ main.go
@@ -4,17 +4,69 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	bannerReader()
-	checkBannerExist()
-	readBannerLine()
-	batchReader()
+	//bannerReader()
+	//checkBannerExist()
+	//readBannerLine()
+	//batchReader()
+	content, err := os.ReadFile("standard.txt")
+	if err != nil {
+		fmt.Println("file does not exist...")
+	}
+
+	charMap := contentSpliter(string(content))
+	//fmt.Println(charMap)
+
+	result := getCharacter("hi", charMap)
+	for _, line := range printAsciiRow(result) {
+		fmt.Println(line)
+	}
+
+	fmt.Println(splitLine("hello\\nworld"))
 }
 
-func contentSpliter() {
-	content, err := os.ReadFile("sample.txt")
+func splitLine(line string) []string {
+	lines := strings.Split(line, "\\n")
+
+	return lines
+}
+
+// row print
+func printAsciiRow(charMap [][]string) []string {
+	row := make([]string, 8)
+
+	for _, block := range charMap {
+		for i := 0; i < 8; i++ {
+			row[i] += block[i]
+		}
+	}
+
+	return row
+}
+
+// 2D storage for characters
+func getCharacter(str string, charMap map[rune][]string) [][]string {
+	words := [][]string{}
+
+	for _, char := range str {
+		words = append(words, charMap[char])
+	}
+	return words
+}
+
+func contentSpliter(content string) map[rune][]string {
+	lines := strings.Split(string(content), "\n")
+	characterMap := map[rune][]string{}
+
+	for i := 32; i <= 126; i++ {
+		startLine := (i - 32) * 9
+		characterMap[rune(i)] = lines[startLine+1 : startLine+9]
+	}
+
+	return characterMap
 }
 
 //File System (fs) API
