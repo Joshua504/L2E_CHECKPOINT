02be96d2f871b2c1913b0961872e5e6c0421e8c1
chuokoh <chukwuebukajosh8@gmail.com>
2026-06-29 10:38:19 +0100

    ascii-web project

--- ascii-art/main.go
+++ ascii-art/main.go
@@ -1,140 +1,61 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	//bannerReader()
-	//checkBannerExist()
-	//readBannerLine()
-	//batchReader()
-	content, err := os.ReadFile("standard.txt")
-	if err != nil {
-		fmt.Println("file does not exist...")
+	if len(os.Args) != 2 {
+		fmt.Println("Error: not enough Arguments___ Usage: go run . <text>")
 	}
 
-	charMap := contentSpliter(string(content))
-	//fmt.Println(charMap)
-
-	result := getCharacter("!", charMap)
-	for _, line := range printAsciiRow(result) {
-		fmt.Println(line)
+	text := os.Args[1]
+	if text == "" {
+		return
 	}
 
-	fmt.Println(splitLine("hello\\nworld"))
-}
+	banner, err := os.ReadFile("thinkertoy.txt")
+	errorHandling(err)
 
-func splitLine(line string) []string {
-	lines := strings.Split(line, "\\n")
+	toString := string(banner)
+	toString = strings.ReplaceAll(toString, "\r\n", "\n")
 
-	return lines
-}
-
-// row print
-func printAsciiRow(charMap [][]string) []string {
-	row := make([]string, 8)
-
-	for _, block := range charMap {
-		for i := 0; i < 8; i++ {
-			row[i] += block[i]
-		}
-	}
-
-	return row
-}
-
-// 2D storage for characters
-func getCharacter(str string, charMap map[rune][]string) [][]string {
-	words := [][]string{}
-
-	for _, char := range str {
-		words = append(words, charMap[char])
-	}
-	return words
-}
-
-func contentSpliter(content string) map[rune][]string {
-	lines := strings.Split(string(content), "\n")
-	characterMap := map[rune][]string{}
+	splitBanner := strings.Split(toString, "\n")
+	charMap := map[rune][]string{}
 
 	for i := 32; i <= 126; i++ {
 		startLine := (i - 32) * 9
-		characterMap[rune(i)] = lines[startLine+1 : startLine+9]
+		charLine := splitBanner[startLine+1 : startLine+9]
+		charMap[rune(i)] = charLine
 	}
+	lines := strings.Split(text, "\n")
 
-	return characterMap
-}
-
-//File System (fs) API
-
-func bannerReader() {
-	content, err := os.ReadFile("sample.txt")
-	if err != nil {
-		fmt.Println("no such file or directory", err)
-	}
-
-	fmt.Println(string(content))
-}
-
-func checkBannerExist() {
-	open, err := os.Open("sample.txt")
-	if err != nil {
-		fmt.Println("Error: file not found", err)
-	}
-	defer func() {
-		if err := open.Close(); err != nil {
-			fmt.Println("Error: can not close the file")
-		}
-	}()
-}
-
-func readBannerLine() {
-	banner, err := os.Open("sample.txt")
-	if err != nil {
-		fmt.Println("no such file or directory", err)
-	}
-	defer func() {
-		if err := banner.Close(); err != nil {
-			fmt.Println("Error: can not close the file")
-		}
-	}()
-
-	scanner := bufio.NewScanner(banner)
-	var store []string
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		store = append(store, line)
-	}
-
-	fmt.Println(store)
-}
-
-func batchReader() {
-	fileName := []string{"sample.txt", "test.txt"}
-
-	for _, file := range fileName {
-		banner, err := os.Open(file)
-		if err != nil {
-			fmt.Println("no such file or directory", err)
+	for i, line := range lines {
+		if line == "" {
+			if i < len(lines)-1 {
+				fmt.Println()
+			}
 			continue
 		}
 
-		scanner := bufio.NewScanner(banner)
-		count := 0
+		row := make([]string, 8)
 
-		for scanner.Scan() {
-			scanner.Text()
-			count++
+		for _, char := range line {
+			for i := 0; i < 8; i++ {
+				row[i] += charMap[char][i]
+			}
 		}
 
-		banner.Close()
-		line := fmt.Sprintf("%s contains %d lines\n", file, count)
-		fmt.Print(line)
+		for _, r := range row {
+			fmt.Println(r)
+		}
+	}
+}
+
+func errorHandling(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
